470136c9c5edbb60ca0d43d95a31f1f52509a5f6
Asteway Nigussie <asteway17@gmail.com>
2025-06-05 13:47:30 -0500

    update insert update delete exercise

--- insert-update-delete/Exercise-2.sql
+++ insert-update-delete/Exercise-2.sql
@@ -3,13 +3,20 @@ INSERT INTO suppliers
 VALUES (NULL, "Asteway Inc", "Asteway", "Sales Agent", "102 King St", "Dallas", "TX", "75055", "USA", "(469) 258-7067", NULL, NULL);
 
 -- 2. Add a new product provided by that supplier.
-SELECT SupplierID INTO @AstewayID 
+/*
+SELECT SupplierID AS AstewayID 
 FROM suppliers 
 WHERE CompanyName = 'Asteway Inc';
-
+*/
 INSERT INTO products
-VALUES (NULL, "Sweet Cheese", @AstewayID, 4, "10 Slices", 4, 55, 25, 10, 0);
-
+VALUES (NULL, "Sweet Cheese", 36, 4, "10 Slices", 4, 55, 25, 10, 0);
+/*
+SET @supplier_id = (
+  SELECT id
+  FROM suppliers
+  WHERE company_name = 'Acme Inc.'
+);
+*/
 -- 3. List all products and their suppliers. 
 SELECT
 	P.ProductName
@@ -22,7 +29,7 @@ JOIN
 -- 4. Raise the price of your new product by 15%. 
 UPDATE products
 SET UnitPrice = 1.15 * UnitPrice
-WHERE ProductID = 79;
+WHERE ProductID = 81;
 
 -- 5. List the products and prices of all products from that supplier. 
 SELECT
@@ -37,11 +44,11 @@ WHERE
 
 -- 6. Delete the new product. 
 DELETE FROM products
-WHERE ProductID = 79;
+WHERE ProductID = 81;
 
 -- 7. Delete the new supplier. 
 DELETE FROM suppliers
-WHERE SupplierID = @AstewayID;
+WHERE SupplierID = 36;
 
 -- 8. List all products. 
 SELECT ProductName
